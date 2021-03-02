98168d2631870417264a92e7839b1cecde8fcca0
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-02 19:09:57 +0100

    Update script3.ps1
    update11

--- script3.ps1
+++ script3.ps1
@@ -7,3 +7,7 @@ net use
 [System.IO.DriveInfo]::getdrives()
 get-wmiobject win32_logicaldisk
 Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==
+net stop nfsclnt
+net stop nfsrdr
+net start nfsrdr 
+net start nfsclnt

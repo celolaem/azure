fbca68f672081a1bca100421fcdea38278149fe9
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-02 19:01:31 +0100

    Update script3.ps1
    update10

--- script3.ps1
+++ script3.ps1
@@ -1,3 +1,6 @@
 sc start NfsRdr
 Get-PSDrive -PSprovider FileSystem
+net use
+[System.IO.DriveInfo]::getdrives()
+get-wmiobject win32_logicaldisk
 Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==

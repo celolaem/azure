209521aa460f7026a1cabc7c6571be9a4221d764
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-02 15:23:26 +0100

    Update script3.ps1
    update3

--- script3.ps1
+++ script3.ps1
@@ -1,7 +1,5 @@
-Param (
-    [string]$user='Azure\csb100320010455505d',
-    [string]$password='lSLc6wzJx4VrdOA8q0S7YlK1Qis+Q5XXpU3Pyan4JlVSaN63IsAmZD7TKKbDZz1nEVXGPrQIewRMpK8+iEiu5g==' 
-)
+$user = 'Azure\csb100320010455505d'
+$storageAccountName = 'lSLc6wzJx4VrdOA8q0S7YlK1Qis+Q5XXpU3Pyan4JlVSaN63IsAmZD7TKKbDZz1nEVXGPrQIewRMpK8+iEiu5g=='
 # Firewall
 netsh advfirewall firewall add rule name="SMB" dir=in action=allow protocol=TCP localport=445
 #Mapear Z con Azure Files

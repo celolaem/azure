b7272cc2f6bfa21629dba25335271e7482124c26
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-02 15:57:03 +0100

    Update script3.ps1
    update5

--- script3.ps1
+++ script3.ps1
@@ -1,6 +1,5 @@
-$user = 'Azure\csb100320010455505d'
-$password = 'lSLc6wzJx4VrdOA8q0S7YlK1Qis+Q5XXpU3Pyan4JlVSaN63IsAmZD7TKKbDZz1nEVXGPrQIewRMpK8+iEiu5g=='
+$user = 'Azure\carloselola'
 # Firewall
-netsh advfirewall firewall add rule name="SMB" dir=in action=allow protocol=TCP localport=445
+#netsh advfirewall firewall add rule name="SMB" dir=in action=allow protocol=TCP localport=445
 #Mapear Z con Azure Files
-Net use Z: \\csb100320010455505d.file.core.windows.net\cs-carlos-elola-bosonit-com-100320010455505d /user:'$user' $password
+Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:$user qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==

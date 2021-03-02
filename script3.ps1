702dde2b13fe6618db9fa9d5c958ac44c1662d31
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-02 17:32:11 +0100

    Update script3.ps1
    update7

--- script3.ps1
+++ script3.ps1
@@ -1,7 +1,1 @@
-$user = 'Azure\carloselola'
-# Firewall
-#netsh advfirewall firewall add rule name="SMB" dir=in action=allow protocol=TCP localport=445
-#Mapear Z con Azure Files
-#Get-PSDrive -Z FileSystem
-Get-PSDrive Z
-Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:$user qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==
+Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==

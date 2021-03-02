682111db156c5c473940b9faca7f8c5a85462767
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-02 18:58:51 +0100

    Update script3.ps1
    update9

--- script3.ps1
+++ script3.ps1
@@ -1,2 +1,3 @@
+sc start NfsRdr
 Get-PSDrive -PSprovider FileSystem
 Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==

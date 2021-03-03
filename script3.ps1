657705da6ae0ba333676035ded77bfdb096ced69
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-03 12:25:38 +0100

    Update script3.ps1
    update12

--- script3.ps1
+++ script3.ps1
@@ -1,4 +1,4 @@
-$connectTestResult = Test-NetConnection -ComputerName carloselola.file.core.windows.net -Port 445
+Start-Sleep -s 300; $connectTestResult = Test-NetConnection -ComputerName carloselola.file.core.windows.net -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Montar la unidad
     Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==; New-Item C:\Users\tfadmin\Desktop\etest.txt; Set-Content C:\Users\tfadmin\Desktop\etest.txt 'Exito Test!'

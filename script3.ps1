4126620e783d70de9339e0ca8d8b58d383db3459
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-03 11:55:33 +0100

    Update script3.ps1
    update12

--- script3.ps1
+++ script3.ps1
@@ -1,3 +1,7 @@
-# Mapear unidad Azure File Share
-Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==
-
+$connectTestResult = Test-NetConnection -ComputerName carloselola.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Montar la unidad
+    Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==; New-Item C:\Users\tfadmin\Desktop\etest.txt; Set-Content C:\Users\tfadmin\Desktop\etest.txt 'Exito Test!'
+} else {
+    New-Item C:\Users\tfadmin\Desktop\testfail.txt; Set-Content C:\Users\tfadmin\Desktop\testfail.txt 'Fail Test 445!'
+}

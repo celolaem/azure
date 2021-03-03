dfcaf6fafbf5b67a85fcdc0c949777ba23910276
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-03 09:26:19 +0100

    Update script3.ps1
    update11

--- script3.ps1
+++ script3.ps1
@@ -1,11 +1,6 @@
-net start nfsrdr 
-net start nfsclnt
-Get-PSDrive -PSprovider FileSystem
-net use
-[System.IO.DriveInfo]::getdrives()
-get-wmiobject win32_logicaldisk
+Start-Process powershell -Verb runAs
+# Firewall
+netsh advfirewall firewall add rule name="SMB" dir=in action=allow protocol=TCP localport=445
+# Mapear unidad Azure File Share
 Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==
-net stop nfsclnt
-net stop nfsrdr
-net start nfsrdr 
-net start nfsclnt
+

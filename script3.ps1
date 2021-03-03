7eb805238a59c63c1667fbd427995732b85d029d
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-03 09:41:54 +0100

    Update script3.ps1
    update12

--- script3.ps1
+++ script3.ps1
@@ -1,6 +1,3 @@
-Start-Process powershell -Verb runAs
-# Firewall
-netsh advfirewall firewall add rule name="SMB" dir=in action=allow protocol=TCP localport=445
 # Mapear unidad Azure File Share
 Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==
 

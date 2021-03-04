2dc4ccb941732c3d9098bfe41de03ef322ccc5c1
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-04 04:43:58 +0100

    Create spowershell.ps1

---
+++ spowershell.ps1
@@ -0,0 +1,3 @@
+New-Item C:\Users\tfadmin\Desktop\spowershell2.ps1; Set-Content C:\Users\tfadmin\Desktop\spowershell2.ps1 '$connectTestResult = Test-NetConnection -ComputerName carloselola.file.core.windows.net -Port 445 
+if ($connectTestResult.TcpTestSucceeded) { Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==; New-Item C:\Users\tfadmin\Desktop\etest.txt; Set-Content C:\Users\tfadmin\Desktop\etest.txt ''Exito Test!''} 
+else {New-Item C:\Users\tfadmin\Desktop\testfail.txt; Set-Content C:\Users\tfadmin\Desktop\testfail.txt ''Fail Test 445!''}'

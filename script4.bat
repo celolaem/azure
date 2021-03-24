a9fc51d4fd7d52aabec203336f575cee6b4fed59
celolaem <78151172+celolaem@users.noreply.github.com>
2021-03-24 17:37:25 +0100

    Update script4.bat

--- script4.bat
+++ script4.bat
@@ -1,3 +1,3 @@
 Net use Z: \\carloselola.file.core.windows.net\azurefiles /user:Azure\carloselola qBP+AZvd9C73eVpWnilH2UBwX+xFd6s0+zHOXlppLAKCZMrFq23Jh6tMKn+uHIel3r8U7o8p2E8F0RuBkxcgtg==
 
-95ba0b83-0c52-43af-bbe3-24bfdda7b764:1uJ0U8aVVKOvMcrE41FTMFJogy/fc/M159AUoyLftII=:c3FsLXNlcnZpZG9yLmRhdGFiYXNlLndpbmRvd3MubmV04oGHVGVzdEJE
+95ba0b83-0c52-43af-bbe3-24bfdda7b764:NQmY/lyi9o++tULxuAJCZEnNLbrS9I2miLlhxCsFI0Y=:c3FsLXNlcnZpZG9yLmRhdGFiYXNlLndpbmRvd3MubmV04oGHVGVzdEJE
